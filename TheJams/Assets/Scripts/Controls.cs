bf85e1fd06da688e4c8db3f94b6dcf8d36227abd
Tobbun <tobbun@gmail.com>
2015-01-26 23:34:56 +0100

    What I managed to finish by the end of GGJ
    figured out animation, figured out some other stuff. Will play around
    with it more over time.

--- TheJams/Assets/Scripts/Controls.cs
+++ TheJams/Assets/Scripts/Controls.cs
@@ -5,7 +5,8 @@ public class Controls : MonoBehaviour {
 	public float sideSpeed;
 	public float jumpspeed = 400;
 	public bool isJumping = false;
-	
+	bool isWalking = false;
+
 	bool mainMusicPlaying = false;
 
 	void Awake () {
@@ -27,14 +28,17 @@ public class Controls : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		Move (KeyCode.D, new Vector2 (1, 0), sideSpeed);
 
+		Move (KeyCode.D, new Vector2 (1, 0), sideSpeed);
 		Move (KeyCode.A, new Vector2 (-1, 0), sideSpeed);
 	}
 
 	private void Move(KeyCode code, Vector3 movement, float moveSpeed){
 		if (Input.GetKey(code)) {
 			rigidbody2D.AddForce (movement * moveSpeed);
+			isWalking = true;
+		} else {
+			isWalking = false;
 		}
 	}
 }
