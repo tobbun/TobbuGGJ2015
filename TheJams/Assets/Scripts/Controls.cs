5e355fa0820eb0c3e2694a00aeb3d69740d20a4b
Torbjørn Brekke <tobbun@gmail.com>
2015-02-03 15:31:46 +0100

    Some more stuff
    Followed a bit of a tutorial on 2D-controllers. Now it moves properly.
    Ish.

--- TheJams/Assets/Scripts/Controls.cs
+++ TheJams/Assets/Scripts/Controls.cs
@@ -2,19 +2,28 @@
 using System.Collections;
 
 public class Controls : MonoBehaviour {
+	public float maxSpeed = 10f;
 	public float sideSpeed;
 	public float jumpspeed = 400;
 	public bool isJumping = false;
+	bool facingRight = true;
 	bool isWalking = false;
 
+	Animator anim;
+
 	bool mainMusicPlaying = false;
+
 
 	void Awake () {
 		// Load the Fabric manager by loading up the Audio scene!
 		AudioManager.LoadFabric();
 	}
 	
+	void Start (){
+		anim = GetComponent<Animator> ();
 	
+	
+	}
 	void Update () {
 		if (!mainMusicPlaying) {
 			if (AudioManager.FabricLoaded) {
@@ -24,21 +33,41 @@ public class Controls : MonoBehaviour {
 //				AudioManager.PlaySound("FX/Amb/Waves-Light");
 			}
 		}
+
+
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
+		float move = Input.GetAxis ("Horizontal");
 
-		Move (KeyCode.D, new Vector2 (1, 0), sideSpeed);
-		Move (KeyCode.A, new Vector2 (-1, 0), sideSpeed);
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+		if (move > 0 && !facingRight) {
+			Flip ();
+		} else if (move < 0 && facingRight) {
+			Flip ();
+		}
+
+		/*Move (KeyCode.D, new Vector2 (1, 0), sideSpeed);
+		Move (KeyCode.A, new Vector2 (-1, 0), sideSpeed);*/
 	}
 
-	private void Move(KeyCode code, Vector3 movement, float moveSpeed){
+	void Flip(){
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	/*private void Move(KeyCode code, Vector3 movement, float moveSpeed){
 		if (Input.GetKey(code)) {
 			rigidbody2D.AddForce (movement * moveSpeed);
 			isWalking = true;
 		} else {
 			isWalking = false;
 		}
-	}
+	}*/
 }
